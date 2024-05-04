c7aa3694b7770a82cd1cfab89d1c28487f1abc51
Carlos Vigueras <carlosj.vigueras@gmail.com>
2024-05-04 17:25:40 +0200

    feat: implemented get_message_over_eighteen_under_thirty_years

--- Object.Mother.App/Models/Home.cs
+++ Object.Mother.App/Models/Home.cs
@@ -8,6 +8,10 @@
             {
                 return "Aplicación web solo para personas mayores de edad";
             }
+            if(user.Age < 30)
+            {
+                return "Consigue un descuento de 20€ en tu primer pedido";
+            }
             return string.Empty;
         }
     }
