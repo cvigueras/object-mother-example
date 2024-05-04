98220ca34a82fec0d118f968d1b29fd319ad32de
Carlos Vigueras <carlosj.vigueras@gmail.com>
2024-05-04 17:09:28 +0200

    chore: init app.

---
+++ Object.Mother.Test/UserShould.cs
@@ -0,0 +1,16 @@
+namespace Object.Mother.Test
+{
+    public class UserShould
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
